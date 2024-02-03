a57383468100f29465ce6feab31ca539bfe9dff5
viizzo67 <viizzo67@gmail.com>
2024-02-03 09:11:22 -0500

    Initial Commit

---
+++ Koduah_Icebreaker_s24/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Koduah_Icebreaker_s24
+//
+//  Created by user on 2/3/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Koduah_Icebreaker_s24/Koduah_Icebreaker_s24App.swift
@@ -0,0 +1,17 @@
+//
+//  Koduah_Icebreaker_s24App.swift
+//  Koduah_Icebreaker_s24
+//
+//  Created by user on 2/3/24.
+//
+
+import SwiftUI
+
+@main
+struct Koduah_Icebreaker_s24App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
